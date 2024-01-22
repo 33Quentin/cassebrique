120f267c3559b30bb75b37c496af51862bda6dc5
33Quentin <138811269+33Quentin@users.noreply.github.com>
2024-01-22 17:54:36 +0100

    Update main.js
    change import three

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-// import * as THREE from 'three';
-import * as THREE from './node_modules/three/build/three.module.js';
+ import * as THREE from 'three';
+//import * as THREE from './node_modules/three/build/three.module.js';
 
 
 const canvas = document.getElementById('myCanvas');
